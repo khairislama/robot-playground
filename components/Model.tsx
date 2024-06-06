6685d45a2e67cbae12967849fe2693204413f916
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-06-06 11:58:19 +0100

    feat: :sparkles: Experiment some new animations and postprocessing

--- components/Model.tsx
+++ components/Model.tsx
@@ -12,20 +12,36 @@ export default function Model() {
   );
   const { actions, clips } = useAnimations(animations, scene);
   const scroll = useScroll();
+
+  // ******************** CREATE THE ANIMATION *******************
+  // ? First we need to pause it
   useEffect(() => {
     //@ts-ignore
     actions["Experiment"].play().paused = true;
-  }, []);
+    //@ts-ignore
+    // actions["Walk"].play().paused = true;
+  }, [actions]);
+  // ? Then we need to attach it to scroll event
   useFrame(
     () =>
       //@ts-ignore
       (actions["Experiment"].time =
         //@ts-ignore
         (actions["Experiment"]?.getClip().duration * scroll.offset) / 1)
+    //@ts-ignore
+    // (actions["Walk"].time =
+    //@ts-ignore
+    // (actions["Walk"].getClip().duration * scroll.offset) / 1)
   );
   return (
-    <group ref={group}>
+    <group ref={group} position={[0, -1.5, 1.5]} scale={[1.25, 1.25, 1.25]}>
       <primitive object={scene} />
+      {/* <mesh
+        geometry={nodes.MeshName.geometry}
+        material={nodes.MeshName.material}
+        onClick={() => console.log("Mesh clicked")}
+        onPointerOver={() => console.log("Pointer over")}
+      /> */}
     </group>
   );
 }
